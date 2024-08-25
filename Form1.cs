b097b0b635b852b6e9b3b9004c317e665e0fba13
CLAVi7 <145520523+CLAVi7@users.noreply.github.com>
2024-08-25 17:29:18 -0600

    estela

--- Form1.cs
+++ Form1.cs
@@ -3,95 +3,101 @@ using System.Windows.Forms;
 
 namespace Tron
 {
-    public partial class Form1 : Form
+public partial class Form1 : Form
+{
+    private Nodo[,] matriz;
+    private Moto moto;
+    private int gridSize = 65; // Tamaño de la matriz 65x65
+    private bool juegoTerminado = false; // Variable para controlar el estado del juego
+
+    public Form1()
     {
-        private Nodo[,] matriz;
-        private Moto moto;
-        private int gridSize = 65; // Tamaño de la matriz 23x23
+        InitializeComponent();
+        CrearMatriz(gridSize, gridSize);
+        DibujarMatriz();
 
-        public Form1()
-        {
-            InitializeComponent();
-            CrearMatriz(gridSize, gridSize);
-            DibujarMatriz();
+        // Inicializa la moto en una posición específica
+        Casilla posicionInicial = matriz[4, 6].Casilla; // Posición central
+        moto = new Moto(posicionInicial, velocidad: 1, tamañoEstela: 4, matriz, this); // Pasar 'this'
 
-            // Inicializa la moto en una posición específica
-            Casilla posicionInicial = matriz[gridSize / 2, gridSize / 2].Casilla; // Posición central
-            moto = new Moto(posicionInicial, velocidad: 1, tamañoEstela: 3, matriz);
+        // Configura la dirección inicial de la moto (por ejemplo, hacia la derecha)
+        moto.Direccion = Direction.Right;
 
-            // Configura la dirección inicial de la moto (por ejemplo, hacia la derecha)
-            moto.Direccion = Direction.Right;
+        // Configura el Timer (usa el definido en el designer)
+        timer.Interval = 100; // Intervalo en milisegundos (ajusta según sea necesario)
+        timer.Tick += Timer_Tick;
+        timer.Start();
+    }
 
-            // Configura el Timer (usa el definido en el designer)
-            timer.Interval = 100; // Intervalo en milisegundos (ajusta según sea necesario)
-            timer.Tick += Timer_Tick;
-            timer.Start();
-        }
-
-        private void CrearMatriz(int filas, int columnas)
-        {
-            matriz = new Nodo[filas, columnas];
-
-            // Crear nodos y conectarlos
-            for (int i = 0; i < filas; i++)
-            {
-                for (int j = 0; j < columnas; j++)
-                {
-                    Casilla casilla;
-                    if (i == 0 || i == filas - 1 || j == 0 || j == columnas - 1)
-                    {
-                        casilla = new Pared(); // Bordes son paredes
-                    }
-                    else
-                    {
-                        casilla = new Libre(); // Interior es libre
-                    }
-
-                    matriz[i, j] = new Nodo(casilla);
-
-                    if (i > 0)
-                    {
-                        matriz[i, j].Arriba = matriz[i - 1, j];
-                        matriz[i - 1, j].Abajo = matriz[i, j];
-                    }
-
-                    if (j > 0)
-                    {
-                        matriz[i, j].Izquierda = matriz[i, j - 1];
-                        matriz[i, j - 1].Derecha = matriz[i, j];
-                    }
-                }
-            }
-        }
-
-        private void DibujarMatriz()
-        {
-            int casillaSize = 10; // Ajusta el tamaño si es necesario
-            this.ClientSize = new System.Drawing.Size(gridSize * casillaSize, gridSize * casillaSize);
-
-            for (int i = 0; i < gridSize; i++)
-            {
-                for (int j = 0; j < gridSize; j++)
-                {
-                    Casilla casilla = matriz[i, j].Casilla;
-                    casilla.Width = casillaSize;
-                    casilla.Height = casillaSize;
-                    casilla.Left = j * casillaSize;
-                    casilla.Top = i * casillaSize;
-                    this.Controls.Add(casilla);
-                }
-            }
-        }
-
-        private void Timer_Tick(object sender, EventArgs e)
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+        if (!juegoTerminado)
         {
             moto.Mover();
         }
+    }
 
-        protected override void OnKeyDown(KeyEventArgs e)
+    private void CrearMatriz(int filas, int columnas)
+    {
+        matriz = new Nodo[filas, columnas];
+
+        // Crear nodos y conectarlos
+        for (int i = 0; i < filas; i++)
         {
-            base.OnKeyDown(e);
+            for (int j = 0; j < columnas; j++)
+            {
+                Casilla casilla;
+                if (i == 0 || i == filas - 1 || j == 0 || j == columnas - 1)
+                {
+                    casilla = new Pared(); // Bordes son paredes
+                }
+                else
+                {
+                    casilla = new Libre(); // Interior es libre
+                }
 
+                matriz[i, j] = new Nodo(casilla);
+
+                if (i > 0)
+                {
+                    matriz[i, j].Arriba = matriz[i - 1, j];
+                    matriz[i - 1, j].Abajo = matriz[i, j];
+                }
+
+                if (j > 0)
+                {
+                    matriz[i, j].Izquierda = matriz[i, j - 1];
+                    matriz[i, j - 1].Derecha = matriz[i, j];
+                }
+            }
+        }
+    }
+
+    private void DibujarMatriz()
+    {
+        int casillaSize = 10; // Ajusta el tamaño si es necesario
+        this.ClientSize = new System.Drawing.Size(gridSize * casillaSize, gridSize * casillaSize);
+
+        for (int i = 0; i < gridSize; i++)
+        {
+            for (int j = 0; j < gridSize; j++)
+            {
+                Casilla casilla = matriz[i, j].Casilla;
+                casilla.Width = casillaSize;
+                casilla.Height = casillaSize;
+                casilla.Left = j * casillaSize;
+                casilla.Top = i * casillaSize;
+                this.Controls.Add(casilla);
+            }
+        }
+    }
+
+    protected override void OnKeyDown(KeyEventArgs e)
+    {
+        base.OnKeyDown(e);
+
+        if (!juegoTerminado) // Solo permitir cambios de dirección si el juego no ha terminado
+        {
             switch (e.KeyCode)
             {
                 case Keys.Up:
@@ -108,6 +114,15 @@ namespace Tron
                     break;
             }
         }
-
     }
+
+    // Método para detener el juego
+    public void DetenerJuego()
+    {
+        juegoTerminado = true;
+        timer.Stop(); // Detener el temporizador
+        MessageBox.Show("¡Perdiste!", "Fin del Juego", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        
+    }
+}
 }
--- moto.cs
+++ moto.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Drawing;
+using System.Windows.Forms;
 
 namespace Tron
 {
@@ -11,12 +12,13 @@ namespace Tron
         public List<string> Items { get; set; }
         public List<string> Poderes { get; set; }
         public LinkedList<Casilla> Estela { get; set; }
-        public Casilla PosicionActual { get; set; }
         public Direction Direccion { get; set; }
         public Nodo[,] Matriz { get; set; }
-        private bool haMovido;
+        public LinkedListNode<Casilla> Head { get; private set; }
+        private Form1 form;
+        private bool juegoTerminado;
 
-        public Moto(Casilla posicionInicial, int velocidad, int tamañoEstela, Nodo[,] matriz)
+        public Moto(Casilla posicionInicial, int velocidad, int tamañoEstela, Nodo[,] matriz, Form1 form)
         {
             Velocidad = velocidad;
             TamañoEstela = tamañoEstela;
@@ -25,62 +27,65 @@ namespace Tron
             Poderes = new List<string>();
             Estela = new LinkedList<Casilla>();
             Matriz = matriz;
+            this.form = form; // Asignar el formulario
+            juegoTerminado = false;
 
             // Inicializar la moto en la posición inicial
-            PosicionActual = posicionInicial;
-            PosicionActual.BackColor = Color.Red; // Color de la moto
+            Head = Estela.AddFirst(posicionInicial);
+            Head.Value.BackColor = Color.Red; // Color de la moto
             Direccion = Direction.Right; // Dirección inicial
-            haMovido = false; // La moto aún no se ha movido
         }
 
         public void Mover()
         {
-            //if (Combustible <= 0)
-               // return; // No mover si no hay combustible
+            if (juegoTerminado)
+                return; // No hacer nada si el juego ya terminó
 
             // Mueve la moto en la dirección actual
             Casilla nuevaPosicion = ObtenerNuevaPosicion();
 
+            if (nuevaPosicion is Pared)
+            {
+                form.DetenerJuego(); // Detener el juego
+                juegoTerminado = true; // Actualizar el estado del juego
+                return;
+            }
+
             // Si la nueva posición es válida, actualizar la posición actual y estela
             if (nuevaPosicion != null)
             {
-                // Limpiar la posición actual de la moto
-                PosicionActual.BackColor = Color.Black;
+                // Añadir la nueva posición al frente de la lista
+                Estela.AddFirst(nuevaPosicion);
+                nuevaPosicion.BackColor = Color.Red; // Color de la moto
 
-                // Solo añadir estela después del primer movimiento
-                if (haMovido)
+                // Actualizar el `Head`
+                Head = Estela.First;
+
+                // Si la estela es más larga que el tamaño permitido, eliminar el último nodo
+                if (Estela.Count > TamañoEstela)
                 {
-                    AñadirEstela(nuevaPosicion);
+                    LinkedListNode<Casilla> nodoAntiguo = Estela.Last;
+                    Estela.RemoveLast();
+                    nodoAntiguo.Value.BackColor = Color.Black; // Restaurar el color anterior
                 }
 
-                // Mover la moto a la nueva posición
-                PosicionActual = nuevaPosicion;
-                PosicionActual.BackColor = Color.Red; // Color de la moto
-
-                // Marcar que la moto se ha movido
-                haMovido = true;
+                // Cambiar el color de la estela (los nodos que no son la moto)
+                foreach (var nodo in Estela)
+                {
+                    if (nodo != Head.Value)
+                    {
+                        nodo.BackColor = Color.Yellow; // Color de la estela
+                    }
+                }
 
                 Combustible--; // Reducir el combustible
             }
         }
 
-        private void AñadirEstela(Casilla casilla)
-        {
-            if (Estela.Count >= TamañoEstela)
-            {
-                Casilla casillaAntigua = Estela.First.Value;
-                Estela.RemoveFirst();
-                casillaAntigua.BackColor = Color.Black; // Restaurar el color anterior
-            }
-
-            Estela.AddLast(casilla);
-            casilla.BackColor = Color.Yellow; // Color de la estela
-        }
-
         private Casilla ObtenerNuevaPosicion()
         {
-            int x = PosicionActual.Left / 10; // Tamaño de la casilla (ajustar según sea necesario)
-            int y = PosicionActual.Top / 10;
+            int x = Head.Value.Left / 10; // Tamaño de la casilla (ajustar según sea necesario)
+            int y = Head.Value.Top / 10;
 
             switch (Direccion)
             {
@@ -93,13 +98,12 @@ namespace Tron
             // Verificar límites del grid
             if (x < 0 || x >= Matriz.GetLength(1) || y < 0 || y >= Matriz.GetLength(0))
             {
-                // La moto ha salido del grid, puedes agregar lógica para manejar este caso (p.ej., detener el juego)
                 return null;
             }
 
             return Matriz[y, x].Casilla; // Obtener la nueva casilla
         }
     }
-
 }
+
 
